fdf8770ee93b360fc6d8d4e786fc379cbf679d7e
Andrei <andrei_sid@yahoo.com>
2019-07-23 16:50:58 +0300

    Update install_validator.sh

--- install_validator.sh
+++ install_validator.sh
@@ -6,10 +6,11 @@ sudo npm install pm2@latest -g
 curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v0.16.5/install/solana-install-init.sh | sh -s
 
 #as a prerequisite you must attach a disk and mout it in /validator_data/
-#sudo fdisk /dev/sdb; sudo mkfs.ext4 /dev/sdb1; sudo mount /dev/sdb1 /validator_data/
-#sudo fdisk /dev/nvme1n1; sudo mkfs.ext4 /dev/nvme1n1p1; 
+#gcp: sudo fdisk /dev/sdb; sudo mkfs.ext4 /dev/sdb1; sudo mount /dev/sdb1 /validator_data/
+#aws: sudo fdisk /dev/nvme1n1; sudo mkfs.ext4 /dev/nvme1n1p1; 
 sudo mkdir /validator_data
-#sudo chown ubuntu /validator_data/
+sudo chown ubuntu /validator_data/
+#gcp: sudo mount /dev/nvme1n1p1 /validator_data/
 sudo mount /dev/nvme1n1p1 /validator_data/
 
 solana-install init --data-dir /validator_data/
@@ -17,10 +18,10 @@ export PATH="/validator_data/active_release/bin:$PATH"
 
 solana-keygen new -o /validator_data/validator-keypair.json
 clear-config.sh
-solana-wallet -k /validator_data/validator-keypair.json airdrop 800000
+# solana-wallet -k /validator_data/validator-keypair.json airdrop 800000
 
-#for now we modify the fullnode.sh to use pm2
-sed -i 's/\$program "\${args\[@\]}"/pm2 start \$program -- "\${args\[@\]}"/g' /validator_data/active_release/multinode-demo/fullnode.sh
+#deprecated
+#sed -i 's/\$program "\${args\[@\]}"/pm2 start \$program -- "\${args\[@\]}"/g' /validator_data/active_release/multinode-demo/fullnode.sh
 
 #should use tds.solana.com 
 ##validator.sh --identity /validator_data/validator-keypair.json --no-airdrop --stake 500000 testnet.solana.com
@@ -39,3 +40,5 @@ module.exports = {
   }],
 };
 EOF
+
+pm2 start

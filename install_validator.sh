8ad23d25392c6852cd32ce8ceea705988f082e5e
Andrei <andrei_sid@yahoo.com>
2019-07-23 17:45:16 +0300

    Update install_validator.sh

--- install_validator.sh
+++ install_validator.sh
@@ -8,15 +8,16 @@ curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v0.16.5/install/s
 #as a prerequisite you must attach a disk and mout it in /validator_data/
 #gcp: sudo fdisk /dev/sdb; sudo mkfs.ext4 /dev/sdb1; sudo mount /dev/sdb1 /validator_data/
 #aws: sudo fdisk /dev/nvme1n1; sudo mkfs.ext4 /dev/nvme1n1p1; 
-sudo mkdir /validator_data
+
+#sudo mkdir /validator_data
 sudo chown ubuntu /validator_data/
 #gcp: sudo mount /dev/nvme1n1p1 /validator_data/
-sudo mount /dev/nvme1n1p1 /validator_data/
+#aws sudo mount /dev/nvme1n1p1 /validator_data/
 
 solana-install init --data-dir /validator_data/
 export PATH="/validator_data/active_release/bin:$PATH"
 
-solana-keygen new -o /validator_data/validator-keypair.json
+#solana-keygen new -o /validator_data/validator-keypair.json
 clear-config.sh
 # solana-wallet -k /validator_data/validator-keypair.json airdrop 800000
 
@@ -25,6 +26,8 @@ clear-config.sh
 
 #should use tds.solana.com 
 ##validator.sh --identity /validator_data/validator-keypair.json --no-airdrop --stake 500000 testnet.solana.com
+#sudo mkdir /var/log/pm2
+#sudo chown ubuntu:ubuntu /var/log/pm2
 cat <<EOF >> ecosystem.config.js
 module.exports = {
   apps : [{

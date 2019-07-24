e7275a4045c4c224ac1b42e10946857aa0e1ba67
Andrei <andrei_sid@yahoo.com>
2019-07-24 11:33:55 +0300

    Update install_validator.sh

--- install_validator.sh
+++ install_validator.sh
@@ -3,33 +3,26 @@ sudo apt-get update
 sudo apt-get install -y npm
 sudo npm install pm2@latest -g
 
-curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v0.16.5/install/solana-install-init.sh | sh -s
+curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v0.16.6/install/solana-install-init.sh | sh -s
 export PATH="/home/ubuntu/.local/share/solana/install/active_release/bin:$PATH"
 
-#as a prerequisite you must attach a disk and mout it in /validator_data/
-#gcp: sudo fdisk /dev/sdb; sudo mkfs.ext4 /dev/sdb1; sudo mount /dev/sdb1 /validator_data/
+#as a prerequisite you must attach a disk and mout it in ~/validator-config
+#gcp: sudo fdisk /dev/sdb; sudo mkfs.ext4 /dev/sdb1; 
 #aws: sudo fdisk /dev/nvme1n1; sudo mkfs.ext4 /dev/nvme1n1p1; 
 
-#sudo mkdir /validator_data
-sudo chown ubuntu /validator_data/
-#gcp: sudo mount /dev/nvme1n1p1 /validator_data/
-#aws sudo mount /dev/nvme1n1p1 /validator_data/
+#mkdir ~/validator-config
+#gcp: sudo mount /dev/nvme1n1p1 ~/validator-config
+#aws sudo mount /dev/nvme1n1p1 ~/validator-config
 
+#solana-keygen new -o ~/validator-config/validator-keypair.json
+# solana-wallet -k ~/validator-config/validator-keypair.json airdrop 800000
 
-solana-install init --data-dir /validator_data/
-export PATH="/validator_data/active_release/bin:$PATH"
-
-rm -rf /home/ubuntu/.local/share/solana
-
-#solana-keygen new -o /validator_data/validator-keypair.json
-clear-config.sh
-# solana-wallet -k /validator_data/validator-keypair.json airdrop 800000
 
 #deprecated
 #sed -i 's/\$program "\${args\[@\]}"/pm2 start \$program -- "\${args\[@\]}"/g' /validator_data/active_release/multinode-demo/fullnode.sh
 
 #should use tds.solana.com 
-##validator.sh --identity /validator_data/validator-keypair.json --no-airdrop --stake 500000 testnet.solana.com
+##validator.sh --identity ~/validator-keypair.json --config-dir ~/validator-config --rpc-port 8899 --poll-for-new-genesis-block testnet.solana.com
 #sudo mkdir /var/log/pm2
 #sudo chown ubuntu:ubuntu /var/log/pm2
 cat <<EOF >> ecosystem.config.js
@@ -37,12 +30,12 @@ module.exports = {
   apps : [{
     name: "validator",
     //for staring without solana-install
-    //script: "fullnode.sh",
-    //cwd: "/validator_data/active_release/multinode-demo/",
-    //args: ["--validator", "--identity", "/validator_data/validator-keypair.json", "--stake", "500000", "testnet.solana.com"],
-    script: "solana-install",
-    cwd: "/validator_data/active_release/bin/",
-    args: ["run", "validator.sh", "--", "--identity", "/validator_data/validator-keypair.json", "--stake", "500000", "testnet.solana.com"],
+    script: "fullnode.sh",
+    cwd: "/home/ubuntu/.local/share/solana/install/active_release/multinode-demo/",
+    args: ["--validator", "--identity", "~/validator-keypair.json", "--config-dir", "~/validator-config", "--rpc-port", "8899", "--poll-for-new-genesis-block", "testnet.solana.com"],
+    //script: "solana-install",
+    //cwd: "/validator_data/active_release/bin/",
+    //args: ["run", "validator.sh", "--", "--identity", "/validator_data/validator-keypair.json", "--stake", "500000", "testnet.solana.com"],
     error_file: "/var/log/pm2/validator-err.log",
     out_file: "/var/log/pm2/validator-out.log",
     merge_logs: true,

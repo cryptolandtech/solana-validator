76b549eaa5c7deaeac3824821ff658be500b2e7d
Andrei <andrei_sid@yahoo.com>
2019-09-23 15:27:23 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps : [{
     name: "solana-validator",
     cwd: "/home/ubuntu/.local/share/solana/install/active_release/bin",
-    script: "solana-validator",
-    args: ["--identity",  "/home/ubuntu/validator-keypair.json", "--voting-keypair", "/home/ubuntu/validator-vote-keypair.json", "--ledger", "/home/ubuntu/volume/validator-config/", "--rpc-port", "8899", "--entrypoint", "edge.testnet.solana.com:8001"],
+    script: "solana-validator-cuda",
+    args: ["--identity",  "/home/ubuntu/validator-keypair.json", "--voting-keypair", "/home/ubuntu/validator-vote-keypair.json", "--ledger", "/home/ubuntu/volume/validator-config/", "--rpc-port", "8899", "--entrypoint", "tds.solana.com:8001"],
     error_file: "/var/log/pm2/validator-err.log",
     out_file: "/var/log/pm2/validator-out.log",
     log_file: "/var/log/pm2/validator-combined.log",
@@ -11,6 +11,11 @@ module.exports = {
     autorestart: true,
     min_uptime: "100s",
     watch: false,
+    env: {
+          "SOLANA_CUDA": "1",
+          "NDEBUG": "1",
+          "LD_LIBRARY_PATH": "/home/ubuntu/.local/share/solana/install/active_release/target/perf-libs:/home/ubuntu/.local/share/solana/install/active_release/target/perf-libs/cuda-10.1:/usr/local/cuda-10.1/lib64:"
+        }
     //max_memory_restart: '1G',
   }],
 };

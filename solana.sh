e22e79209c8d24914360e024f3ae281f685ff661
Andrei <andrei_sid@yahoo.com>
2019-10-29 19:59:33 +0200

    Update solana.sh

--- solana.sh
+++ solana.sh
@@ -253,7 +253,18 @@ solana --keypair ~/validator-keypair.json --url http://tds.solana.com:8899 deleg
 #sudo mkdir /var/log/pm2; sudo chown ubuntu:ubuntu /var/log/pm2
 
 ###Install nvidia
-
+sudo rm /etc/apt/sources.list.d/cuda*
+sudo apt remove --autoremove nvidia-cuda-toolkit
+sudo apt remove --autoremove nvidia-*
+sudo apt update
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
+sudo apt update
+sudo apt install cuda-10-1
+dpkg -l |grep nvidia
+nvidia-smi 
 ############
 
 
@@ -321,6 +332,8 @@ solana show-vote-account ~/validator-vote-keypair.json
 #solana withdraw-stake
 #Once your deactivated stake has cooled down, solana withdraw-stake can be used to reclaim those lamports back
 
+#show cluster version
+solana cluster-version --url http://tds.solana.com:8899/
 
 
 

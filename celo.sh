911eb2811a77e9c73ee67dbdcf645a0fcc0d3496
Andrei <andrei_sid@yahoo.com>
2019-11-27 15:10:27 +0200

    Update celo.sh

--- celo.sh
+++ celo.sh
@@ -1,5 +1,6 @@
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common python
+sudo apt install apt-transport-https ca-certificates curl software-properties-common python npm 
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
@@ -7,3 +8,47 @@ apt-cache policy docker-ce
 sudo apt install docker-ce
 sudo systemctl status docker
 sudo usermod -aG docker ${USER}
+sudo npm install -g @celo/celocli
+
+docker pull us.gcr.io/celo-testnet/celo-node:alfajores
+mkdir celo-data-dir
+cd celo-data-dir
+
+#create account
+#docker run -v `pwd`:/root/.celo --entrypoint /bin/sh -it us.gcr.io/celo-testnet/celo-node:alfajores -c "geth account new"
+docker run -v `pwd`:/root/.celo --entrypoint /bin/sh -it us.gcr.io/celo-testnet/celo-node:alfajores
+geth account new
+export CELO_ACCOUNT_ADDRESS=e741722652bca8870c4ea15bf11aa1990fff266e
+echo "export CELO_ACCOUNT_ADDRESS=e741722652bca8870c4ea15bf11aa1990fff266e" >> ~/.bashrc
+
+#get genesis
+echo "export CELO_ACCOUNT_ADDRESS=e741722652bca8870c4ea15bf11aa1990fff266e" >> ~/.bashrc
+
+#add peer
+docker run -v `pwd`:/root/.celo --entrypoint cp us.gcr.io/celo-testnet/celo-node:alfajores /celo/static-nodes.json /root/.celo/
+
+#start node
+docker run -d -p 127.0.0.1:8545:8545 -p 127.0.0.1:8546:8546 -p 30303:30303 -p 30303:30303/udp -v `pwd`:/root/.celo us.gcr.io/celo-testnet/celo-node:alfajores --verbosity 3 --networkid 44785 --syncmode full --rpc --rpcaddr 0.0.0.0 --rpcapi eth,net,web3,debug,admin,personal --lightserv 90 --lightpeers 1000 --maxpeers 1100 --etherbase $CELO_ACCOUNT_ADDRESS
+
+#run a validator
+#pull docker image
+docker pull us.gcr.io/celo-testnet/celo-node:alfajores
+
+#create 2 more accounts
+cd ~/celo-dir-data
+docker run -v `pwd`:/root/.celo --entrypoint /bin/sh -it us.gcr.io/celo-testnet/celo-node:alfajores 
+geth account new
+
+docker run -v `pwd`:/root/.celo --entrypoint /bin/sh -it us.gcr.io/celo-testnet/celo-node:alfajores 
+geth account new
+
+echo "export CELO_VALIDATOR_GROUP_ADDRESS=" >> ~/.bashrc
+echo "export CELO_VALIDATOR_ADDRESS=" >> ~/.bashrc
+source ~/.bashrc
+
+#generate proof of posesion
+$ docker run -v `pwd`:/root/.celo --entrypoint /bin/sh -it us.gcr.io/celo-testnet/celo-node:alfajores -c "geth account proof-of-possession $CELO_VALIDATOR_ADDRESS"
+echo "export CELO_VALIDATOR_POP=" >> ~/.bashrc
+source ~/.bashrc
+
+

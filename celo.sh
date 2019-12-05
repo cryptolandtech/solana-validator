a5f27f89bf6cc217d2bf58ce928672ae0e0c98bd
Andrei <andrei_sid@yahoo.com>
2019-12-05 21:10:27 +0200

    Update celo.sh

--- celo.sh
+++ celo.sh
@@ -16,20 +16,25 @@ mkdir celo-data-dir
 cd celo-data-dir
 
 #create account
-#docker run -v `pwd`:/root/.celo --entrypoint /bin/sh -it us.gcr.io/celo-testnet/celo-node:alfajores -c "geth account new"
-docker run -v `pwd`:/root/.celo --entrypoint /bin/sh -it us.gcr.io/celo-testnet/celo-node:alfajores
-geth account new
-export CELO_ACCOUNT_ADDRESS=e741722652bca8870c4ea15bf11aa1990fff266e
-echo "export CELO_ACCOUNT_ADDRESS=e741722652bca8870c4ea15bf11aa1990fff266e" >> ~/.bashrc
+#docker run -v $PWD:/root/.celo --entrypoint /bin/sh -it $CELO_IMAGE -c "sleep 1 && geth account new"
+
+export CELO_ACCOUNT_ADDRESS=ea267df8fbbbcd33acc8e87290e34a4d55691744
+echo "export CELO_ACCOUNT_ADDRESS=ea267df8fbbbcd33acc8e87290e34a4d55691744" >> ~/.bashrc
 
 #get genesis
-echo "export CELO_ACCOUNT_ADDRESS=e741722652bca8870c4ea15bf11aa1990fff266e" >> ~/.bashrc
+docker run -v $PWD:/root/.celo $CELO_IMAGE init /celo/genesis.json
 
 #add peer
-docker run -v `pwd`:/root/.celo --entrypoint cp us.gcr.io/celo-testnet/celo-node:alfajores /celo/static-nodes.json /root/.celo/
+docker run -v $PWD:/root/.celo --entrypoint cp $CELO_IMAGE /celo/static-nodes.json /root/.celo/
 
 #start node
-docker run -d -p 127.0.0.1:8545:8545 -p 127.0.0.1:8546:8546 -p 30303:30303 -p 30303:30303/udp -v `pwd`:/root/.celo us.gcr.io/celo-testnet/celo-node:alfajores --verbosity 3 --networkid 44785 --syncmode full --rpc --rpcaddr 0.0.0.0 --rpcapi eth,net,web3,debug,admin,personal --lightserv 90 --lightpeers 1000 --maxpeers 1100 --etherbase $CELO_ACCOUNT_ADDRESS
+docker run --name celo-fullnode -d --restart always -p 127.0.0.1:8545:8545 -p 127.0.0.1:8546:8546 -p 30303:30303 -p 30303:30303/udp -v $PWD:/root/.celo $CELO_IMAGE --verbosity 3 --networkid $NETWORK_ID --syncmode full --rpc --rpcaddr 0.0.0.0 --rpcapi eth,net,web3,debug,admin,personal --lightserv 90 --lightpeers 1000 --maxpeers 1100 --etherbase $CELO_ACCOUNT_ADDRESS
+
+
+
+
+
+
 
 #run a validator
 #pull docker image

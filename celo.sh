525c7ff1b0a543fc5e9a06b1f9208b1b1e098baa
Andrei <andrei_sid@yahoo.com>
2019-12-09 00:49:29 +0200

    Update celo.sh

--- celo.sh
+++ celo.sh
@@ -1,6 +1,6 @@
-sudo apt update
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install apt-transport-https ca-certificates curl software-properties-common python npm nodejs
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common python nodejs
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"

c4a6eefc5c0cbc769b062a318336ea7b8ecc31b3
Andrei <andrei_sid@yahoo.com>
2020-01-26 02:33:16 +0200

    Update nu.sh

--- nu.sh
+++ nu.sh
@@ -1,6 +1,5 @@
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt update
-
-
 sudo apt install python3
 apt install python3-pip
 ln -s /usr/bin/python3 /usr/bin/python
@@ -11,10 +10,13 @@ apt install python3-virtualenv
 apt-get install python3-venv
 apt-get install python3-dev
 apt-get install gcc
+apt install nodejs
+apt-get install python-dev
 
 pip install --upgrade setuptools
 pip install wheel
 pip install cytoolz
+apt install npm
 
 
 #python3 -m venv ./nucypher-venv
@@ -22,12 +24,6 @@ source nucypher-venv/bin/activate
 
 pip3 install -U nucypher
 sudo apt install screen
-screen
-#And create a new console by pressing ctrl+a and c. 
-#View available consoles ctrl+a and w. select console ctrl+a and "console number".
-
-#in each screen
-source nucypher-venv/bin/activate
 
 #install geth
 sudo apt-get install software-properties-common
@@ -35,4 +31,14 @@ sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get install ethereum
 
+screen
+#And create a new console by pressing ctrl+a and c. 
+#View available consoles ctrl+a and w. select console ctrl+a and "console number".
+
+#in each screen
+source nucypher-venv/bin/activate
 geth --goerli --syncmode light
+#DETACH CTRL+A+D
+
+
+

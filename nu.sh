bce8063938437ba6504e960f33dad31f7158024d
Andrei <andrei_sid@yahoo.com>
2020-01-26 02:52:38 +0200

    Update nu.sh

--- nu.sh
+++ nu.sh
@@ -1,17 +1,12 @@
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt update
-sudo apt install python3
-apt install python3-pip
+apt update
+apt install python3 python3-pip libffi-dev python3-dev python3-virtualenv python3-venv python3-dev gcc nodejs python-dev software-properties-common
+add-apt-repository -y ppa:ethereum/ethereum
+apt-get update
+apt-get install ethereum
+
 ln -s /usr/bin/python3 /usr/bin/python
 ln -s /usr/bin/pip3 /usr/bin/pip
-apt install libffi-dev
-apt install python3-dev
-apt install python3-virtualenv
-apt-get install python3-venv
-apt-get install python3-dev
-apt-get install gcc
-apt install nodejs
-apt-get install python-dev
 
 pip install --upgrade setuptools
 pip install wheel
@@ -20,16 +15,9 @@ apt install npm
 
 
 #python3 -m venv ./nucypher-venv
-source nucypher-venv/bin/activate
-
-pip3 install -U nucypher
-sudo apt install screen
-
-#install geth
-sudo apt-get install software-properties-common
-sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo apt-get update
-sudo apt-get install ethereum
+#source nucypher-venv/bin/activate
+#pip3 install -U nucypher
+#sudo apt install screen
 
 screen
 #And create a new console by pressing ctrl+a and c. 
@@ -40,5 +28,31 @@ source nucypher-venv/bin/activate
 geth --goerli --syncmode light
 #DETACH CTRL+A+D
 
+screen
+source nucypher-venv/bin/activate
+
+geth attach /root/.ethereum/goerli/geth.ipc
+personal.newAccount()
+personal.newAccount()
+eth.accounts
+web3.toChecksumAddress(eth.accounts[0])
+web3.toChecksumAddress(eth.accounts[1])
+exit
+
+#Link for requesting a test ETH https://goerli-faucet.slock.it/
+#Link to discord NuCypher-bot  https://discord.gg/CmNNFjn
+.getfunded <your_eth_checksumaddress>
+
+nucypher stake init-stakeholder --provider ipc:///root/.ethereum/goerli/geth.ipc --network cassandra
+nucypher stake create
+nucypher stake restake --enable
+nucypher stake list
+
+
+#Worker
+nucypher ursula init --provider ipc:///home/<your_username>/.ethereum/goerli/geth.ipc --poa --network cassandra --staker-address <your staker address>
+nucypher stake set-worker
+
+nucypher ursula run --teacher discover.nucypher.network:9151 --interactive
 
 
